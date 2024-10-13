0a7fafe496351b566baa91a47bb73e90e8607598
Noevain <noevain@gmail.com>
2024-10-13 19:11:15 +0200

    Add helper class SubscriptionTaskDetails

---
+++ WaymarkPresetPlugin/Subscription/SubscriptionTaskDetails.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WaymarkPresetPlugin.Subscription
+{
+	//from https://github.com/grittyfrog/MacroMate/blob/master/MacroMate/Subscription/SubscriptionTaskDetails.cs
+	///<summary>
+	///Helper class to display information about a subscription task to user
+	///</summary>
+	public class SubscriptionTaskDetails
+	{
+		public Guid Id = Guid.NewGuid();
+		public DateTimeOffset CreatedAt = DateTimeOffset.UtcNow;
+		public string? Message { get; set; } = null;
+
+		public string Summary
+		{
+			get
+			{
+				if (Children.Count > 0)
+				{
+					return Message + "  •  " + Children.OrderBy(c => c.CreatedAt).LastOrDefault()?.Summary ?? "";
+				}
+				else
+				{
+					return Message ?? "";
+				}
+			}
+		}
+
+		private int _loadingCount = 0;
+		public int LoadingCount { get => _loadingCount; }
+
+		public bool IsLoading => LoadingCount > 0 || Children.Any(child => child.IsLoading);
+
+		public async Task<T> Loading<T>(Func<Task<T>> block)
+		{
+			return await Catching(async () => {
+				try
+				{
+					Interlocked.Increment(ref _loadingCount);
+					return await block();
+				}
+				finally
+				{
+					Interlocked.Decrement(ref _loadingCount);
+				}
+			});
+		}
+
+		public async Task Loading(Func<Task> block)
+		{
+			await Catching(async () => {
+				try
+				{
+					Interlocked.Increment(ref _loadingCount);
+					await block();
+				}
+				finally
+				{
+					Interlocked.Decrement(ref _loadingCount);
+				}
+			});
+		}
+
+		public async Task Catching(Func<Task> block)
+		{
+			try
+			{
+				await block();
+			}
+			catch (Exception ex)
+			{
+				var errChainParent = Child(ex.Message);
+				var inner = ex.InnerException;
+				while (inner != null)
+				{
+					errChainParent = errChainParent.Child(inner.Message);
+					inner = inner.InnerException;
+				}
+				throw;
+			}
+		}
+
+		public async Task<T> Catching<T>(Func<Task<T>> block)
+		{
+			try
+			{
+				return await block();
+			}
+			catch (Exception ex)
+			{
+				var errChainParent = Child(ex.Message);
+				var inner = ex.InnerException;
+				while (inner != null)
+				{
+					errChainParent = errChainParent.Child(inner.Message);
+					inner = inner.InnerException;
+				}
+				throw;
+			}
+		}
+
+		public SubscriptionTaskDetails Child(string message)
+		{
+			var childScope = new SubscriptionTaskDetails { Message = message };
+			Children.Add(childScope);
+			return childScope;
+		}
+
+		public ConcurrentBag<SubscriptionTaskDetails> Children = new();
+	}
+}
