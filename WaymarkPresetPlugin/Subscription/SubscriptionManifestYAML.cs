8f32f1d9acc4d391a69a5ff15c0f4a28c8c9ca30
Noevain <noevain@gmail.com>
2024-10-13 14:28:17 +0200

    Added YamlDotNet and proto SubManifest

---
+++ WaymarkPresetPlugin/Subscription/SubscriptionManifestYAML.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YamlDotNet.Serialization;
+using YamlDotNet.Serialization.NamingConventions;
+
+namespace WaymarkPresetPlugin.Subscription
+{
+	public class SubscriptionManifestYAML
+	{
+		public string Name { get; set; } = null!;
+		
+		public List<WaymarkYAML> waymarks {  get; set; } = null!;
+
+		public static SubscriptionManifestYAML From(string input){
+			var deserializer = new DeserializerBuilder()
+			.WithNamingConvention(CamelCaseNamingConvention.Instance)
+			.Build();
+
+			return deserializer.Deserialize<SubscriptionManifestYAML>(input);
+
+		}
+	}
+
+	public class WaymarkYAML
+	{
+		public string? Name { get; set; } = null!;
+		public string? UrlToWaymark { get; set; } = null!;
+	}
+}
